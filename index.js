d2cd7f62f0af93c2039b5b6aa3f9d8011505211c
Kevin Tatooles <tatooles@wisc.edu>
2022-10-16 22:58:23 -0500

    Trying to get stockfish to work

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express');
+const stockfish = require('stockfish');
+const engine = await stockfish();
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
 
+engine.onmessage = function (event) {
+    //NOTE: Web Workers wrap the response in an object.
+    console.log(event.data ? event.data : event);
+};
+
+
+console.log(engine);
+// engine.postMessage("uci");
+
+// run_engine();
+
+// const run_engine = async () => {
+//     await engine().then((val) => {
+
+//     })
+// }
+
 app.get('/bestmove', (req, res) => {
+    // Probably want to use a get with query params
+    // https://stackoverflow.com/questions/6912584/how-to-get-get-query-string-variables-in-express-js-on-node-js
+    // Then use UCI commands to generate the best move and return that
     res.status(200).send({
         piece: 'pawn',
         square: 'e4'
@@ -26,5 +48,5 @@ app.post('/makemove/:move', (req, res) => {
 
 app.listen(
     PORT,
-    () => console.log(`Server running on http://localhost:${PORT}`)
+    () => console.log(`Server running on ${PORT}`)
 );
