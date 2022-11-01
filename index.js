b88a45a9091607837e5a864b0d26e5fc46a1eaed
Kevin Tatooles <tatooles@wisc.edu>
2022-10-31 22:26:42 -0500

    Add deployed url as accepted originn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin: 'http://localhost:5173'
+        origin: ['http://localhost:5173', 'https://play-chess-online.netlify.app']
     })
 )
 
