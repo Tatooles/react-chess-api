850c67ef92d87e37b9fa9b65a350eb349629ad4d
Kevin Tatooles <tatooles@wisc.edu>
2022-11-17 22:35:37 -0600

    Remove unused endpoints

--- handler.js
+++ handler.js
@@ -52,18 +52,6 @@ app.post('/', (req, res) => {
   engine.postMessage(`go depth ${depth}`);
 });
 
-app.get("/", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from from a root that has been updated!",
-  });
-});
-
-app.get("/hello", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from path!",
-  });
-});
-
 app.use((req, res, next) => {
   return res.status(404).json({
     error: "Not Found",
